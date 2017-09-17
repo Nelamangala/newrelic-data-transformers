a799503336ee5ce0803eddf6d1973fc21a9a2cf8
GANESH BABU NELAMANGALA <ganesh.sit@gmail.com>
2017-09-16 22:49:42 -0500

    Basic maven skeleton

---
+++ src/main/java/com/newrelic/datatransformers/NewrelicDataTransformersApplication.java
@@ -0,0 +1,12 @@
+package com.newrelic.datatransformers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewrelicDataTransformersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewrelicDataTransformersApplication.class, args);
+	}
+}
---
+++ src/test/java/com/newrelic/datatransformers/NewrelicDataTransformersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.newrelic.datatransformers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewrelicDataTransformersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
